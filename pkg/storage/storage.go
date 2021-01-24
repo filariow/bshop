3bc373bca7e3e651aaf749ed450114eb5ff8c220
Francesco Ilario <fr.ilario@gmail.com>
2021-01-24 03:28:48 +0100

    Add storage interface

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -7,9 +7,9 @@ import (
 )
 
 type BeerRepository interface {
-	CreateBeer(context.Context, bshop.Beer) (int64, error)
-	ReadBeer(int64) (context.Context, bshop.Beer, error)
-	UpdateBeer(context.Context, bshop.Beer) error
-	DeleteBeer(context.Context, int64) error
-	ListBeers(context.Context) ([]bshop.Beer, error)
+	Create(context.Context, bshop.Beer) (int64, error)
+	Read(context.Context, int64) (bshop.Beer, error)
+	Update(context.Context, bshop.Beer) error
+	Delete(context.Context, int64) error
+	List(context.Context) ([]bshop.Beer, error)
 }
