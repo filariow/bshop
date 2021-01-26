044b1b19a2392e03ac6943ed7f1925412d958b0a
Francesco Ilario <fr.ilario@gmail.com>
2021-01-26 01:56:27 +0100

    Add auxiliary pages

---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        <v-row class="text-center">
+            <v-col class="mb-4">
+                <h1 class="display-2 font-weight-bold mb-3">This pretends to be an Home Page!</h1>
+                <!-- <v-col cols="12">
+                    <v-img src="../../assets/logo.svg" class="my-3" height="200" contain/>
+                </v-col> -->
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script lang="ts">
+    import { Vue, Component } from "vue-property-decorator";
+    @Component({})
+    export default class Home extends Vue {}
+</script>
