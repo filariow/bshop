c3a725144c53b4ea1fa252eb0238bb26cae0a5c3
Francesco Ilario <fr.ilario@gmail.com>
2021-01-26 01:57:41 +0100

    Register routes in router

--- front-end/src/router/index.ts
+++ front-end/src/router/index.ts
@@ -1,9 +1,15 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
   {
     path: "/about",
     name: "About",
@@ -12,6 +18,31 @@ const routes: Array<RouteConfig> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/help",
+    name: "Help",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Help.vue")
+  },
+  {
+    path: "/support",
+    name: "Support",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Support.vue")
+  },
+
+  {
+    path: "/beers",
+    name: "Beers",
+    component: () => 
+      import(/* webpackChunkName: "about" */ "../views/beers/BeersIndex.vue")
   }
 ];
 
